a3bbedb51adb902523617a694866dda1b9d6918f
kizaa123 <adomakoisaac724@gmail.com>
2026-03-12 15:32:11 +0000

    changes on the backend

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const db = require('./database');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5501;
 
 // Middleware
 app.use(cors());
